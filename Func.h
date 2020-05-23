535d6029bfcb063f4838674c16cfda90890838c4
BRKot <65824266+BRKot@users.noreply.github.com>
2020-05-23 21:37:38 +0400

    Add files via upload

---
+++ Func.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+// Отображение шестнадцатеричной цифры
+char digit(int num);
+// Получение целой части числа
+int dectox_int(int a, int p, char* s);
+// Получение дробной части числа
+void dectox_double(double a, int p, char* s);
+// Перевод в 10 СС
+double toSS10(double  value, int ss);
+// Мантиса в 10 СС
+double Man(double &value, int ss);
+// Цел часть в 10 СС
+int Cel(double value, int ss);
